e9064d8daf9367f0d8555a627dd2497277faa78c
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-02 17:34:33 +0530

    UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="bg-red-600 text-red">i'm RED</div>
+    <Navbar/>
+     <div className=" bg-violet-100 container mx-auto my-5 rounded-xl p-5 min-h-[80vh]">
+        <div className="addTodo">
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input type="text" className='bg-white'/>
+          <button className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-6'>Add</button>
+        </div>
+        <h2 className='text-lg font-bold'>Your ToDo'S</h2>
+        <div className="todos">
+          <div className="todo flex">
+            <div className="text"></div>
+            <div className="buttons ">
+              <button className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Edit</button>
+              <button className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Delete</button>
+
+            </div>
+          </div>
+        </div>
+     
+     </div>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between bg-slate-700 text-white py-2">
+        <div className="logo">
+            <span className="font-bold text-xl mx-9">ToDOSync</span>
+        </div>
+      <ul className="flex gap-8 mx-9">
+        <li className="cursor-pointer hover:font-bold transition-shadow duration-75">Home</li>
+        <li className="cursor-pointer hover:font-bold transition-3d duration-75">Your Tasks</li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar
