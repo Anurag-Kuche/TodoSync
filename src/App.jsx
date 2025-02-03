0d6ff975786f29257a32b9f56f2e13ae97a99648
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-03 23:03:02 +0530

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,48 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[todo,setTodo]=useState("")
+  const [todos,setTodos]=useState([])
+
+  const handleEdit=()=>{
+
+  }
+  const handleDelete=()=>{
+
+  }
+
+  const handleAdd=()=>{
+    setTodos([...todos,{todo,isCompleted:false}])
+    setTodo("")
+  }
+
+  const handleChange=(e)=>{
+    setTodo(e.target.value)
+  }
 
   return (
     <>
     <Navbar/>
      <div className=" bg-violet-100 container mx-auto my-5 rounded-xl p-5 min-h-[80vh]">
-        <div className="addTodo">
+        <div className="addTodo my-5">
           <h2 className='text-lg font-bold'>Add a Todo</h2>
-          <input type="text" className='bg-white'/>
-          <button className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-6'>Add</button>
+          <input onClick={handleChange}  value={todo} type="text" className='bg-white w-1/2'/>
+          <button onClick={handleAdd} className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-6'>Add</button>
         </div>
-        <h2 className='text-lg font-bold'>Your ToDo'S</h2>
+        <h2 className='text-lg font-bold'>Your Todos</h2>
         <div className="todos">
-          <div className="todo flex">
-            <div className="text"></div>
+          {todos.map(item=>{
+            <div className="todo flex">
+            <div className="text">{items.todo}</div>
             <div className="buttons ">
-              <button className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Edit</button>
-              <button className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Delete</button>
+              <button onClick={handleEdit} className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Edit</button>
+              <button onClick={handleDelete} className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Delete</button>
 
             </div>
           </div>
+
+          })}
+          
         </div>
      
      </div>
