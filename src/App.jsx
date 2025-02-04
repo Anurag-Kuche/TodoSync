82e3ae1ab2387b4e490c9fc41659ce274f8a417f
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-04 18:05:47 +0530

    delete added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 import Navbar from './components/Navbar'
 
@@ -9,18 +10,36 @@ function App() {
   const handleEdit=()=>{
 
   }
-  const handleDelete=()=>{
+  const handleDelete=(e,id)=>{
+   
+   
+    let newTodos=todos.filter(item=>{
+      return item.id!==id
+    });
+    
+    setTodos(newTodos)
 
   }
 
   const handleAdd=()=>{
-    setTodos([...todos,{todo,isCompleted:false}])
+    setTodos([...todos,{id:uuidv4(),todo,isCompleted:false}])
     setTodo("")
   }
 
   const handleChange=(e)=>{
     setTodo(e.target.value)
   }
+  const handleCheckbox=(e) => {
+    let id=e.target.name;
+    let index=todos.findIndex(item=>{
+      return item.id==id;
+    })
+    let newTodos=[...todos];
+    newTodos[index].isCompleted=!newTodos[index].isCompleted;
+    setTodos(newTodos)
+
+  }
+  
 
   return (
     <>
@@ -34,11 +53,12 @@ function App() {
         <h2 className='text-lg font-bold'>Your Todos</h2>
         <div className="todos">
           {todos.map(item=>{
-            return <div className="todo flex w-1/2 justify-between">
-            <div className={item.isCompleted?"":"line-through"}>{item.todo}</div>
+            return <div key={item.id}className="todo flex w-1/4 justify-between my-3">
+              <input name={item.id} onChange={handleCheckbox} type="checkbox" value={item.isCompleted}  id='' />
+            <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
             <div className="buttons">
               <button onClick={handleEdit} className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Edit</button>
-              <button onClick={handleDelete} className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Delete</button>
+              <button onClick={(e)=>{handleDelete(item.id)}} className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Delete</button>
 
             </div>
           </div>
