c33efe8e521abd3debf4c77e4ae8e6d06fa4b2f4
Anurag-kuche <anuragkuche1@gmail.com>
2025-02-04 11:28:02 +0530

    task display added

--- src/App.jsx
+++ src/App.jsx
@@ -28,15 +28,15 @@ function App() {
      <div className=" bg-violet-100 container mx-auto my-5 rounded-xl p-5 min-h-[80vh]">
         <div className="addTodo my-5">
           <h2 className='text-lg font-bold'>Add a Todo</h2>
-          <input onClick={handleChange}  value={todo} type="text" className='bg-white w-1/2'/>
+          <input onChange={handleChange}  value={todo} type="text" className='bg-white w-1/2'/>
           <button onClick={handleAdd} className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-6'>Add</button>
         </div>
         <h2 className='text-lg font-bold'>Your Todos</h2>
         <div className="todos">
           {todos.map(item=>{
-            <div className="todo flex">
-            <div className="text">{items.todo}</div>
-            <div className="buttons ">
+            return <div className="todo flex w-1/2 justify-between">
+            <div className={item.isCompleted?"":"line-through"}>{item.todo}</div>
+            <div className="buttons">
               <button onClick={handleEdit} className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Edit</button>
               <button onClick={handleDelete} className='bg-violet-500 hover:bg-violet-900 p-2 py-1 text-sm font-bold text-white rounded-lg mx-1'>Delete</button>
 
